98ad3ada82a2001e92c91e161581dc9ef6f7e3c4
Felipe <felipesantos.dsb@hotmail.com>
2023-07-13 14:26:25 -0300

    update files

--- backend/controllers/templeController.js
+++ backend/controllers/templeController.js
@@ -2,6 +2,22 @@ const {Temple: TempleModel } = require("../models/Temple");
 
 const templeController = {
 
+    create: async(req, res) => {
+        try {
+            const temple = {
+                name: req.body.name,
+                address:req.body.address,
+                telephone: req.body.telephone,
+                photoUrl: req.body.photoUrl
+            };
+
+            const response = await TempleModel.create(temple);
+            res.status(201).json({response, msg: "Temple added Successfully!"})
+
+        }catch {
+
+        }
+    }
 }
 
 module.exports = templeController;
--- backend/routes/temples.js
+++ backend/routes/temples.js
@@ -3,32 +3,13 @@ const router = require('express').Router();
 
 const templeController = require("../controllers/templeController");
 
-// Rota GET para buscar todos os templos
-router.get('/', (req, res) => {
-  // Lógica para buscar todos os templos no banco de dados e retornar a resposta
-});
 
-// Rota GET para buscar um templo específico
-router.get('/:id', (req, res) => {
-  const templeId = req.params.id;
-  // Lógica para buscar um templo pelo ID no banco de dados e retornar a resposta
-});
+router
+.route("/temples")
+.post((req, res) => templeController.create(req, res));
+ 
 
-// Rota POST para criar um novo templo
-router.post('/', (req, res) => {
-  // Lógica para criar um novo templo com base nos dados fornecidos na requisição
-});
 
-// Rota PUT para atualizar um templo existente
-router.put('/:id', (req, res) => {
-  const templeId = req.params.id;
-  // Lógica para atualizar um templo pelo ID com base nos dados fornecidos na requisição
-});
 
-// Rota DELETE para deletar um templo existente
-router.delete('/:id', (req, res) => {
-  const templeId = req.params.id;
-  // Lógica para deletar um templo pelo ID do banco de dados
-});
 
 module.exports = router;
