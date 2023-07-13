48fc5ec63d6f9022c3e517097f3228cad7217e93
Felipe <felipesantos.dsb@hotmail.com>
2023-07-13 00:07:20 -0300

    trying to connect with MongoDB Atlas

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 
+
+mongoose
+  .connect('mongodb+srv://<felipesud>:<felipe>@cluster0.lvbamfb.mongodb.net/?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('Connected to MongoDB Atlas');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB Atlas:', error);
+  });
+
 const templeRoutes = require('./routes/temples');
-app.use('./api/temples', templeRoutes);
+app.use('/api/temples', templeRoutes);
 
-app.listen(port, () => console.log(`Server running on port ${port}`))
-
-
+app.listen(port, () => console.log(`Server running on port ${port}`));
