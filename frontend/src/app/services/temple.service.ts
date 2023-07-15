f87101cc2f591544501b60168527df844f6cde26
Felipe <felipesantos.dsb@hotmail.com>
2023-07-14 22:35:09 -0300

    update service

--- frontend/src/app/services/temple.service.ts
+++ frontend/src/app/services/temple.service.ts
@@ -1,32 +1,48 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Temple } from '../models/temple.model';
+import { TempleAdapter } from '../adapters/temple.adapter';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TempleService {
+  private apiUrl: string;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private templeAdapter: TempleAdapter) {
+    this.apiUrl = environment.apiUrl;
+  }
 
   getTemples(): Observable<Temple[]> {
-    return this.http.get<Temple[]>('');
+    return this.http.get<any>(`${this.apiUrl}`).pipe(
+      map((response: any[]) => response.map(item => this.templeAdapter.adapt(item)))
+    );
   }
 
   createTemple(temple: Temple): Observable<Temple> {
-    return this.http.post<Temple>('http://localhost:3000/api/temples', temple);
+    const adaptedTemple = this.templeAdapter.adapt(temple);
+    return this.http.post<any>(`${this.apiUrl}/api/temples`, adaptedTemple).pipe(
+      map((response: any) => this.templeAdapter.adapt(response))
+    );
   }
 
   getTemple(id: string): Observable<Temple> {
-    return this.http.get<Temple>(`http://localhost:3000/api/temples/${id}`);
+    return this.http.get<any>(`${this.apiUrl}/api/temples/${id}`).pipe(
+      map((response: any) => this.templeAdapter.adapt(response))
+    );
   }
 
   updateTemple(id: string, temple: Temple): Observable<Temple> {
-    return this.http.put<Temple>(`http://localhost:3000/api/temples/${id}`, temple);
+    const adaptedTemple = this.templeAdapter.adapt(temple);
+    return this.http.put<any>(`${this.apiUrl}/api/temples/${id}`, adaptedTemple).pipe(
+      map((response: any) => this.templeAdapter.adapt(response))
+    );
   }
 
   deleteTemple(id: string): Observable<any> {
-    return this.http.delete<any>(`http://localhost:3000/api/temples/${id}`);
+    return this.http.delete<any>(`${this.apiUrl}/api/temples/${id}`);
   }
 }
